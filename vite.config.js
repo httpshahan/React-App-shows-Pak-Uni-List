c7b0feb8cc63499cb5e7aea0dab080f4af32cdf1
httpshahan <shahankhushdil@gmail.com>
2024-01-19 22:06:42 +0500

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://httpshahan.github.io/React-App-shows-Pak-Uni-List/',
   plugins: [react()],
 })
