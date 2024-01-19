56b77d4681c94753c413c7d97ffccf2f2f5a9163
httpshahan <shahankhushdil@gmail.com>
2024-01-19 17:15:32 +0500

    base changed in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-App-shows-Pak-Uni-List/',
+  base: 'http://httpshahan.github.io/React-App-shows-Pak-Uni-List/',
   plugins: [react()],
 })
